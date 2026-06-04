469f42936a624e51be446f19c3f8413babd5cbe6
Harsh Guleria <guleriah59@gmail.com>
2026-06-04 21:16:20 +0530

    Refactor promise handling examples and comment out unused code and new promise example

--- js-class/script.js
+++ js-class/script.js
@@ -359,7 +359,7 @@ const GITHUB_API = "https://api.github.com/users/forgewithharsh";
 
 const user = fetch(GITHUB_API);
 
-console.log(user);
+// console.log(user);
 
 // user.then((data) => {
 //   console.log(data);
@@ -380,45 +380,80 @@ const cart = ["shoes", "pants", "kurta"];
 
 // const promise = createOrder(cart);
 
-createOrder(cart)
-  .then(function (orderId) {
-    console.log(orderId);
-    return orderId;
-  })
-  .then(function (orderId) {
-    return proceedToPayment(orderId);
-  })
-  .then(function (paymentInfo) {
-    console.log(paymentInfo);
-  })
-  .catch(function (err) {
-    console.log(err.message);
-  });
+// createOrder(cart)
+//   .then(function (orderId) {
+//     console.log(orderId);
+//     return orderId;
+//   })
+//   .then(function (orderId) {
+//     return proceedToPayment(orderId);
+//   })
+//   .then(function (paymentInfo) {
+//     console.log(paymentInfo);
+//   })
+//   .catch(function (err) {
+//     console.log(err.message);
+//   });
 
-function validateCart(cart) {
-  return true;
+// function validateCart(cart) {
+//   return true;
+// }
+
+// function proceedToPayment(orderId) {
+//   return new Promise(function (resolve, reject) {
+//     resolve("Payment Successfully!");
+//   });
+// }
+
+// function createOrder(cart) {
+//   const pr = new Promise(function (resolve, reject) {
+// Validate Cart
+//     if (!validateCart(cart)) {
+//       const err = new Error("Cart is not valid");
+//       reject(err);
+//     }
+
+// Logic for createOrder
+//     const orderId = "12345";
+//     if (orderId) {
+//       resolve(orderId);
+//     }
+//   });
+
+//   return pr;
+// }
+
+async function dataLao() {
+  let response = await fetch("https://picsum.photos/v2/list");
+
+  let data = await response.json();
+  console.log(data);
 }
+// dataLao();
 
-function proceedToPayment(orderId) {
-  return new Promise(function (resolve, reject) {
-    resolve("Payment Successfully!");
-  });
-}
+let p1 = new Promise((resolve, reject) => {
+  let val = true;
+  if (val) {
+    console.log("Success");
+  } else {
+    console.log("Failure");
+  }
+});
 
-function createOrder(cart) {
-  const pr = new Promise(function (resolve, reject) {
-    // Validate Cart
-    if (!validateCart(cart)) {
-      const err = new Error("Cart is not valid");
-      reject(err);
+let p2 = new Promise(function (resolve, reject) {
+  console.log("Promise Pending hai....");
+
+  let result = true;
+
+  setTimeout(() => {
+    if (result) {
+      console.log("Fulfilled");
+
+      resolve()
+    } else {
+      console.log("Rejected");
+
+      reject()
     }
-
-    // Logic for createOrder
-    const orderId = "12345";
-    if (orderId) {
-      resolve(orderId);
-    }
-  });
-
-  return pr;
-}
+  }, 3000);
+});

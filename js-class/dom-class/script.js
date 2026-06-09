a4642835740788124ab7247b81c1beca54e4fe15
Harsh Guleria <guleriah59@gmail.com>
2026-06-09 22:05:26 +0530

    Add initial HTML and JavaScript files for DOM manipulation examples

---
+++ js-class/dom-class/script.js
@@ -0,0 +1,13 @@
+// NodeList will be reload
+// const h1 = document.querySelector('h1')
+
+// HTMLCollection is live
+// const h1 = document.getElementsByTagName("h1");
+
+// const h1 = document.getElementById('heading')
+
+const h1 = document.getElementsByClassName("hey");
+
+// h1.textContent = 'hey....';
+
+console.log(h1);

18cc65516b2aef7cbe1b4c8ec00e813e3120e366
Harsh Guleria <guleriah59@gmail.com>
2026-06-09 22:30:20 +0530

    Refactor script.js to enable h1 element selection and update its text and background color

--- js-class/dom-class/script.js
+++ js-class/dom-class/script.js
@@ -1,13 +1,17 @@
 // NodeList will be reload
-// const h1 = document.querySelector('h1')
+const h1 = document.querySelector('h1')
 
 // HTMLCollection is live
 // const h1 = document.getElementsByTagName("h1");
 
 // const h1 = document.getElementById('heading')
 
-const h1 = document.getElementsByClassName("hey");
+// const h1 = document.getElementsByClassName("hey");
 
 // h1.textContent = 'hey....';
+
+h1.innerText = "Polo"
+
+h1.style.backgroundColor = "orange"
 
 console.log(h1);

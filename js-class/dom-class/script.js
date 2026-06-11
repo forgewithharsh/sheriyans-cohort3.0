229e0a120a57db87605d1dead13d4fdf8df04e1d
Harsh Guleria <guleriah59@gmail.com>
2026-06-11 21:35:00 +0530

    add dynamic footer, and enhance input button functionality

--- js-class/dom-class/script.js
+++ js-class/dom-class/script.js
@@ -22,4 +22,27 @@ h3.removeAttribute("class");
 
 console.log(h3.hasAttribute("id"));
 
-console.log(res);
+// console.log(res);
+
+const userCard = document.querySelector("#user-card");
+
+userCard.dataset.userId = "678";
+
+console.log(userCard.getAttribute("data-user-id"));
+
+const inp = document.querySelector("input");
+const btn = document.querySelector("button");
+
+btn.addEventListener("click", () => {
+  console.log(inp.value);
+  console.log(inp.getAttribute("value"));
+});
+
+const main = document.querySelector("main");
+
+let footer = document.createElement("footer");
+const span = document.createElement("span")
+
+span.innerHTML = "hey i am <i>dynamic...</i>"
+
+main.append(footer, span);

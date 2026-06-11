8e0b08db60cccdbd0f1d8378e993b142d4c8e626
Harsh Guleria <guleriah59@gmail.com>
2026-06-11 21:08:32 +0530

    Refactor index.html, script.js, and style.css to comment out unused code and clean up structure

--- js-class/dom-class/script.js
+++ js-class/dom-class/script.js
@@ -1,14 +1,25 @@
-const bulb = document.querySelector(".bulb");
-const btn = document.querySelector("button");
+// const bulb = document.querySelector(".bulb");
+// const btn = document.querySelector("button");
 
-let flag = true;
+// let flag = true;
 
-btn.addEventListener("click", () => {
-  let lightBulb = bulb.classList.toggle("lightUp");
+// btn.addEventListener("click", () => {
+//   let lightBulb = bulb.classList.toggle("lightUp");
 
-  if (lightBulb) {
-    btn.textContent = "Off";
-  } else {
-    btn.textContent = "On";
-  }
-});
+//   if (lightBulb) {
+//     btn.textContent = "Off";
+//   } else {
+//     btn.textContent = "On";
+//   }
+// });
+
+const h3 = document.querySelector("#id1");
+
+let res = h3.getAttribute("class");
+// h3.setAttribute("width", "200");
+
+h3.removeAttribute("class");
+
+console.log(h3.hasAttribute("id"));
+
+console.log(res);

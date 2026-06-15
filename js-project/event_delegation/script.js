8bb2ab241698550691a3ae8ef482c67c94f8e44b
Harsh Guleria <guleriah59@gmail.com>
2026-06-15 21:02:27 +0530

    Add initial HTML structure and event delegation script for button clicks

---
+++ js-project/event_delegation/script.js
@@ -0,0 +1,10 @@
+const five = document.querySelector("#five");
+const main = document.querySelector("main");
+
+// five.addEventListener("click", (e) => {
+//   console.log(e.target);
+// });
+
+main.addEventListener("click", (e) => {
+  console.log(e.target);
+});

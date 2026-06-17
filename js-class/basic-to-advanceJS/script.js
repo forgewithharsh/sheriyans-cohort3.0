7cf970d260812d91b677e4667fdc07b531bc0ab0
Harsh Guleria <guleriah59@gmail.com>
2026-06-17 11:13:34 +0530

    variables, data types and operators in js

---
+++ js-class/basic-to-advanceJS/script.js
@@ -0,0 +1,46 @@
+// console.log("This is a hello from external js file!");
+
+var age = 22;
+
+// console.log(age);
+
+age = "harsh";
+
+// console.log(age);
+
+if (true) {
+  var age = 23;
+}
+
+// console.log(age);
+
+// Numbers
+let num = 3.14;
+let num2 = 10;
+
+// String
+let firstName = "123#4harsh";
+const lastName = "Garg";
+
+// Boolean
+let isLoggedIn = true;
+
+// console.log(isLoggedIn);
+
+// Null
+let lastLoginDate = null;
+
+//Undefined
+let x;
+
+// Objects
+const person = {
+  firstName: "harsh",
+  lastName: "guleria",
+  age: 19,
+  isLoggedIn: true,
+};
+
+// console.log(person.age);
+
+

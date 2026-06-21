513c327c5af3bd70934e0494b8dc9e232fb5c23e
Harsh Guleria <guleriah59@gmail.com>
2026-06-21 23:45:29 +0530

    feat: update calculator display initialization and enhance button click handling

--- js-project/calculator/script.js
+++ js-project/calculator/script.js
@@ -1,3 +1,21 @@
+const display = document.querySelector(".display");
+
 function handleClickButton(btn) {
-  console.log(btn.innerText);
+  const value = btn.innerText;
+
+  if (value === "=") {
+    let exp = display.innerText;
+
+    exp = exp
+      .replaceAll("×", "*")
+      .replaceAll("÷", "/")
+      .replaceAll("−", "-");
+
+    display.innerText = eval(exp);
+
+  } else if (value === "AC") {
+    display.innerText = "";
+  } else {
+    display.innerText += value;
+  }
 }

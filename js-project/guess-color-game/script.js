dac67493551a8da404f176ba6c569154ba2683e2
Harsh Guleria <guleriah59@gmail.com>
2026-06-21 23:09:34 +0530

    feat: implement scoring system and update color generation logic in Guess the Color game

--- js-project/guess-color-game/script.js
+++ js-project/guess-color-game/script.js
@@ -1,6 +1,7 @@
 const colorCodeContainer = document.getElementById("color-code");
 let randomColor = null;
 const optionContainer = document.getElementById("options-container");
+let score = 0;
 
 function generateRandomNumbersBetween(min, max) {
   return min + Math.floor(Math.random() * (max - min + 1));
@@ -11,16 +12,32 @@ function generateRandomColorRGB() {
   const blue = generateRandomNumbersBetween(0, 255);
   const green = generateRandomNumbersBetween(0, 255);
 
-  return `${red}, ${green}, ${blue}`;
+  return `rgb(${red}, ${green}, ${blue})`;
+}
+
+function incrementScore() {
+  score++;
+  document.getElementById("score").innerText = score;
+}
+
+function validateResult(el) {
+  const selectedColor = el.target.style.backgroundColor;
+  if (selectedColor === randomColor) {
+    incrementScore()
+  }
 }
 
 function startGame() {
   randomColor = generateRandomColorRGB();
   colorCodeContainer.innerText = randomColor;
 
+  const ansIndex = generateRandomNumbersBetween(0, 5);
+
   for (let i = 0; i < 6; i++) {
     const div = document.createElement("div");
-    div.style.backgroundColor = generateRandomColorRGB();
+    div.addEventListener("click", validateResult);
+    div.style.backgroundColor =
+      i === ansIndex ? randomColor : generateRandomColorRGB();
     optionContainer.append(div);
   }
 }

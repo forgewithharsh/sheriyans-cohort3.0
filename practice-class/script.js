e9452fcc3f06f8e2468bc32a2133f0f23f77913f
Harsh Guleria <guleriah59@gmail.com>
2026-06-22 11:30:21 +0530

    Debounce Utility from Scratch

--- practice-class/script.js
+++ practice-class/script.js
@@ -684,4 +684,26 @@ const miniProject = async () => {
   }
 };
 
-miniProject();
+// miniProject();
+
+function debounce(fn, delay) {
+  let timer;
+
+  return function () {
+    clearInterval(timer);
+
+    timer = setTimeout(() => {
+      fn();
+    }, delay);
+  };
+}
+
+function search() {
+  console.log("Searching...");
+}
+
+const debouncedSearch = debounce(search, 1000);
+// debouncedSearch();
+
+
+

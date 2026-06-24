950fcc44b27c495100f679368f166c34db23d24d
Harsh Guleria <guleriah59@gmail.com>
2026-06-24 21:37:39 +0530

    feat: add About component and integrate it into App component

---
+++ react/day3-jsx_comp/src/About.jsx
@@ -0,0 +1,5 @@
+let About = () => {
+  return <h1>Hey i am about</h1>;
+};
+
+export default About;
--- react/day3-jsx_comp/src/App.jsx
+++ react/day3-jsx_comp/src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
-let App = () => {
+import About from "./About";
 
+let App = () => {
   return (
     <div>
       <h1>hello</h1>
       <h2>bye</h2>
       <h3>vapus aoo</h3>
+      <About/>
     </div>
   );
 };

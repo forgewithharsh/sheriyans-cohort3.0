bd9ad8f91ec9bdb8ec9884b4a78b531bb76668d8
Harsh Guleria <guleriah59@gmail.com>
2026-06-24 21:59:09 +0530

    feat: update About component to accept additional props and render children in App component

--- react/day3-jsx_comp/src/About.jsx
+++ react/day3-jsx_comp/src/About.jsx
@@ -1,9 +1,13 @@
-let About = (props) => {
-  console.log(props);
+let About = ({ width, name, age, element, children }) => {
+  console.log(width, name, age, element);
+
+  console.log(children);
+  
 
   return (
     <nav>
       <h1>Hey i am about</h1>
+      {children}
     </nav>
   );
 };
--- react/day3-jsx_comp/src/App.jsx
+++ react/day3-jsx_comp/src/App.jsx
@@ -7,7 +7,9 @@ let App = () => {
       <h1>hello</h1>
       <h2>bye</h2>
       <h3>vapus aoo</h3>
-      <About width="300" name="harsh"/>
+      <About width="300" name="harsh" age={19} element={<h1>hello</h1>}>
+        <h1>Hello</h1>
+      </About>
     </div>
   );
 };

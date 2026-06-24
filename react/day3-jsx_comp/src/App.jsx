d8959ef139b646810cc08ee4c3c1351fa9a1ed94
Harsh Guleria <guleriah59@gmail.com>
2026-06-24 21:19:28 +0530

    feat: update App component to render dynamic UI elements instead of static string

--- react/day3-jsx_comp/src/App.jsx
+++ react/day3-jsx_comp/src/App.jsx
@@ -1,5 +1,13 @@
+import React from "react";
 let App = () => {
-  return "Hello";
+
+  let ui = React.createElement('div', {} , [
+    React.createElement('h1', {}, 'hello'),
+    React.createElement('h2', {}, 'bye'),
+    React.createElement('h3', {}, 'vapus aao'),
+  ])
+
+  return ui;
 };
 
 export default App;

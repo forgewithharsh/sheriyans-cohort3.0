a0203d5909c9216f15db8806a39ac72bbb9a907c
Harsh Guleria <guleriah59@gmail.com>
2026-06-26 22:13:28 +0530

    feat: add character count functionality to textarea in script.js and update index.html

--- practice-class/js-practice-class/script.js
+++ practice-class/js-practice-class/script.js
@@ -11,12 +11,52 @@
 // })
 
 // Q3.
-const input = document.querySelector("#username");
-const btn = document.querySelector("button");
-const p = document.querySelector("#output");
+// const input = document.querySelector("#username");
+// const btn = document.querySelector("button");
+// const p = document.querySelector("#output");
 
-btn.addEventListener("click", () => {
-  const value = input.value;
+// btn.addEventListener("click", () => {
+//   const value = input.value;
 
-  p.textContent = value;
+//   p.textContent = value;
+// });
+
+// Q4.
+// const btn = document.querySelector("button");
+// const p = document.querySelector("#message");
+
+// btn.addEventListener("click", () => {
+//   if (p.style.display === "block") {
+//     p.style.display = "none";
+//   } else {
+//     p.style.display = "block";
+//   }
+// });
+
+// Q5.
+// const list = document.getElementById("list");
+// const btn = document.querySelector("button");
+
+// btn.addEventListener("click", () => {
+//   const li = document.createElement("li");
+
+//   li.textContent = "Banana";
+
+//   list.appendChild(li);
+// });
+
+// Q6.
+// const btn = document.querySelector("button");
+
+// btn.addEventListener("click", () => {
+//   btn.parentElement.remove();
+// });
+
+// Q7.
+const textArea = document.querySelector("textarea");
+const character = document.querySelector(".char");
+
+textArea.addEventListener("input", () => {
+  let count = textArea.value.length;
+  character.textContent = `Characters: ${count}`;
 });

e895911bef6bfe9f0e1f905ecfa5ea1ebb156df4
Harsh Guleria <guleriah59@gmail.com>
2026-06-28 12:28:13 +0530

    refactor: update HTML structure and enhance CSS styles for bird game; remove unused bottom pipe image

--- bird-game/script.js
+++ bird-game/script.js
@@ -6,11 +6,27 @@ let gravity = 2;
 
 setInterval(() => {
   birdTop += gravity;
-  bird.style.top = birdTop + "px"
-}, 20)
+  bird.style.top = birdTop + "px";
+}, 20);
 
-document.addEventListener('keydown', (e) => {
-  if(e.code === "Enter"){
-    birdTop = birdTop - 60
+document.addEventListener("keydown", (e) => {
+  if (e.code === "Space") {
+    birdTop = birdTop - 80;
   }
-})
+});
+
+function createPipe() {
+  const pipeTop = document.createElement("div");
+  const pipeBottom = document.createElement("div");
+
+  pipeTop.className = "pipe";
+  pipeBottom.className = "pipe";
+
+  let maxheight = game.clientHeight;
+
+  let gap = 150;
+
+  let topPipeHeight = Math.random() * maxheight + 50;
+
+  // let BottomPipeHeight =
+}

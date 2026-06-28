720334706d9f0e98207fda8dac2c0d38cfd12174
Harsh Guleria <guleriah59@gmail.com>
2026-06-28 11:49:36 +0530

    Refactor assignment 8 JavaScript and CSS files for improved readability and maintainability; update bird game styles and functionality with new background image and event handling.

--- bird-game/script.js
+++ bird-game/script.js
@@ -6,39 +6,11 @@ let gravity = 2;
 
 setInterval(() => {
   birdTop += gravity;
-  bird.style.top = birdTop + "px";
-}, 20);
+  bird.style.top = birdTop + "px"
+}, 20)
 
-document.addEventListener("keydown", (e) => {
-  if (e.code === "Space") {
-    birdTop = birdTop - 60;
+document.addEventListener('keydown', (e) => {
+  if(e.code === "Enter"){
+    birdTop = birdTop - 60
   }
-});
-
-function createPipe() {
-  const pipeTop = document.createElement("div");
-  const pipeBottom = document.createElement("div");
-
-  pipeTop.className = "pipe";
-  pipeBottom.className = "pipe";
-
-  let gap = 100;
-
-  let gameHeight = game.clientHeight;
-
-  let maxHeight = gameHeight - gap - 50;
-
-
-  let topPipeHeight = Math.random() * maxHeight + 50;
-  let bottomPipeHeight = maxHeight - topPipeHeight - gap;
-
-  pipeTop.style.height = topPipeHeight + "px";
-  pipeBottom.style.height = bottomPipeHeight + "px"
-
-  pipeTop.style.top = 0
-  pipeBottom.style.bottom = 0
-
-  game.append(pipeTop, pipeBottom)
-
-  // let pipeLeft =
-}
+})

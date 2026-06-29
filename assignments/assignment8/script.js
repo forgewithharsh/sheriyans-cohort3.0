a547ccd632e349111e6398051a1144171ad174d7
Harsh Guleria <guleriah59@gmail.com>
2026-06-29 12:02:21 +0530

    refactor: enhance user authentication functions and improve local storage handling

--- assignments/assignment8/script.js
+++ assignments/assignment8/script.js
@@ -1,12 +1,26 @@
 // Local Storage
 
 function getRegisteredUsers() {
-  return JSON.parse(localStorage.getItem('reg-users') || '[]')
+  return JSON.parse(localStorage.getItem("reg-users") || "[]");
 }
 
-function saveRegisteredUsers(arr){
-  localStorage.setItem('reg-users', JSON.stringify(arr))
+function saveRegisteredUsers(arr) {
+  localStorage.setItem("reg-users", JSON.stringify(arr));
 }
+
+function getActiveSession() {
+  return JSON.parse(localStorage.getItem("active-users") || "null");
+}
+
+function saveActiveSession(user) {
+  localStorage.setItem("active-users", JSON.stringify(user));
+}
+
+function clearActiveSession(){
+  sessionStorage.removeItem('active-users')
+}
+
+
 
 // Login and Register
 
@@ -23,28 +37,28 @@ function showAuthScreen(scr) {
   }
 }
 
-function doRegister(){
+function doRegister() {
   const username = document.getElementById("register-username").value.trim();
   const password = document.getElementById("register-password").value;
 
-  if(!username || !password){
-    alert('Please fill all the fields.')
-    return
+  if (!username || !password) {
+    alert("Please fill all the fields.");
+    return;
   }
 
   const users = getRegisteredUsers();
 
-  if(users.find(user => user.username === username)){
+  if (users.find((user) => user.username === username)) {
     alert("Username already taken");
     return;
   }
 
   users.push({
     username,
-    password
-  })
+    password,
+  });
 
-   saveRegisteredUsers(users);
+  saveRegisteredUsers(users);
 
   alert("Registered Successfully!");
 
@@ -52,20 +66,44 @@ function doRegister(){
   document.getElementById("register-password").value = "";
 
   showAuthScreen("login");
-
-
 }
 
 function doLogin() {
-   const username = document.getElementById("login-username").value.trim();
+  const username = document.getElementById("login-username").value.trim();
   const password = document.getElementById("login-password").value;
 
   if (!username || !password) {
-    alert('Please fill all the fields.')
+    alert("Please fill all the fields.");
     return;
   }
 
+  const found = getRegisteredUsers().find(
+    (user) => user.username === username && user.password === password,
+  );
 
+  if (!found) {
+    alert("Wrong username or password");
+    return;
+  }
 
+  saveActiveSession({ username });
 
+  startApp();
 }
+
+function doLogout() {
+  clearActiveSession()
+  document.getElementById('app-screen').classList.remove('on');
+
+  document.getElementById('auth-screen').style.display = '';
+
+  showAuthScreen('login')
+}
+
+function startApp(){
+  document.getElementById('auth-screen').style.display = 'none';
+
+  document.getElementById("app-screen").classList.add("on");
+}
+
+

582d0753c0329dfc47fb1211eb9e7b410e11e81b
Harsh Guleria <guleriah59@gmail.com>
2026-07-02 23:29:36 +0530

    feat: add ProductsCard component and implement product rendering in App

---
+++ react/day5-state_rendering/src/ProductsCard.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const ProductsCard = ({ product }) => {
+  return (
+    <div className="p-2 border-2 flex flex-col gap-4 rounded">
+      <div className="w-40">
+        <img src={product.image} alt="" />
+      </div>
+      <div>
+        <h2 className="font-semibold">{product.title.substring(0, 20)}</h2>
+        <p className="text-xs">{product.category}</p>
+        <p className="text-green-600">{product.price}</p>
+      </div>
+      <button className="p-2 bg-red-500">Delete</button>
+    </div>
+  );
+};
+
+export default ProductsCard;

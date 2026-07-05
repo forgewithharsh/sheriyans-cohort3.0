839643b733a5a00567b5a855f6c1c487ff860aea
Harsh Guleria <guleriah59@gmail.com>
2026-07-05 09:31:40 +0530

    feat: create Web component and add Card, Contact, and About components

---
+++ react/day6-rendering-formHandling/src/Web.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Card from "./components/Card";
+import Contact from "./components/Contact";
+import About from "./components/About";
+
+const Web = () => {
+  return (
+    <div>
+      <h1>Web</h1>
+      <Card />
+      <Contact />
+      <About />
+    </div>
+  );
+};
+
+export default Web;

0764cc289b08e86b21b21802c6a0a8523d971605
Harsh Guleria <guleriah59@gmail.com>
2026-07-05 19:01:15 +0530

    feat: update Web component to render multiple Card components from an array

--- react/day6-rendering-formHandling/src/Web.jsx
+++ react/day6-rendering-formHandling/src/Web.jsx
@@ -4,9 +4,13 @@ import Contact from "./components/Contact";
 import About from "./components/About";
 
 const Web = () => {
+  let arr = [2, 3, 4, 5, 6, 7, 8, 9, 0, 8, 7, 6, 5, 4, 3, 2, 1];
+
   return (
     <div className="">
-      <Card />
+      {arr.map((index) => {
+        return <Card key={index}/>
+      })}
     </div>
   );
 };

ebc71e37d9b160a660e8a0dc181ee0898ff75bcf
Harsh Guleria <guleriah59@gmail.com>
2026-07-06 01:10:36 +0530

    feat: implement name input handling in Web component

--- react/day6-rendering-formHandling/src/Web.jsx
+++ react/day6-rendering-formHandling/src/Web.jsx
@@ -1,16 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "./components/Card";
 import Contact from "./components/Contact";
 import About from "./components/About";
 
 const Web = () => {
+  const [name, setName] = useState("");
+  console.log(name);
 
   return (
-    <div className="flex flex-col gap-5 w-60 m-auto">
-      <input className="border-2" type="text" placeholder="Name"/>
-      <input className="border-2" type="text" placeholder="Email"/>
-      <input className="border-2" type="text" placeholder="Password"/>
-      <button className="border-2">Submit</button>
+    <div className="flex flex-col gap-5 w-60">
+      <input
+        onChange={(e) => setName(e.target.value)}
+        className="border-2"
+        type="text"
+        placeholder="Name"
+      />
+
+      <h1>This is Name - {name}</h1>
     </div>
   );
 };

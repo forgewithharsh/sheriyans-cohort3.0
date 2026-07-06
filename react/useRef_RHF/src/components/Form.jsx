15c1d9a9437f0d6720a2e3bf8c9ac1a46a2fe102
Harsh Guleria <guleriah59@gmail.com>
2026-07-06 21:20:33 +0530

    feat: implement form submission handling and refactor input structure in Form component

--- react/useRef_RHF/src/components/Form.jsx
+++ react/useRef_RHF/src/components/Form.jsx
@@ -1,28 +1,46 @@
-import React, { useState } from "react";
+import React, { useRef } from "react";
 
 const Form = () => {
-  const [formData, setFormData] = useState({});
+  const inpRef = useRef();
+  console.log(inpRef);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
 
   return (
     <div className="w-80 h-screen">
-      <form className="flex p-6 rounded bg-white flex-col gap-4">
+      <form
+        onSubmit={handleSubmit}
+        className="flex p-6 rounded bg-white flex-col gap-4"
+      >
         <input
+          // onChange={(e) =>
+          // setFormData({ ...formData, product: e.target.value })
+          // }
           className="p-2 border border-gray-400 rounded"
           type="text"
           placeholder="Product name"
         />
         <input
+          // onChange={(e) => setFormData({ ...formData, price: e.target.value })}
           className="p-2 border border-gray-400 rounded"
           type="text"
           placeholder="Price"
         />
         <span>Select category:</span>
-        <select className="p-2 border border-gray-400 rounded">
+        <select
+          // onChange={(e) =>
+          //   setFormData({ ...formData, category: e.target.value })
+          // }
+          className="p-2 border border-gray-400 rounded"
+        >
           <option value="mens">Mens</option>
           <option value="women">Women</option>
           <option value="kids">Kids</option>
         </select>
         <input
+          // onChange={(e) => setFormData({ ...formData, image: e.target.value })}
           className="p-2 border border-gray-400 rounded"
           type="text"
           placeholder="Image"

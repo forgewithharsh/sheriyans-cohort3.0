622803be0026a859b897398176f8d09f6bf1912b
Harsh Guleria <guleriah59@gmail.com>
2026-07-06 21:12:27 +0530

    feat: initialize React project with Vite and Tailwind CSS, leanring useRef() and Real DOM and Virtual DOM

---
+++ react/useRef_RHF/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Form from "./components/Form";
+
+const App = () => {
+  return (
+    <div className="h-screen bg-gray-300 p-5 w-full">
+      <h1 className="mb-8">Hey this is form</h1>
+      <Form/>
+    </div>
+  );
+};
+
+export default App;
---
+++ react/useRef_RHF/src/components/Form.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+
+const Form = () => {
+  const [formData, setFormData] = useState({});
+
+  return (
+    <div className="w-80 h-screen">
+      <form className="flex p-6 rounded bg-white flex-col gap-4">
+        <input
+          className="p-2 border border-gray-400 rounded"
+          type="text"
+          placeholder="Product name"
+        />
+        <input
+          className="p-2 border border-gray-400 rounded"
+          type="text"
+          placeholder="Price"
+        />
+        <span>Select category:</span>
+        <select className="p-2 border border-gray-400 rounded">
+          <option value="mens">Mens</option>
+          <option value="women">Women</option>
+          <option value="kids">Kids</option>
+        </select>
+        <input
+          className="p-2 border border-gray-400 rounded"
+          type="text"
+          placeholder="Image"
+        />
+        <button className="p-2 bg-blue-600 text-white rounded">CREATE</button>
+      </form>
+    </div>
+  );
+};
+
+export default Form;

a874e555890fa53adf3bbf74d7b06b6ca79c4e03
Harsh Guleria <guleriah59@gmail.com>
2026-07-06 08:08:24 +0530

    better approch than previous brute force approach

--- react/day6-rendering-formHandling/src/Web.jsx
+++ react/day6-rendering-formHandling/src/Web.jsx
@@ -4,36 +4,34 @@ import Contact from "./components/Contact";
 import About from "./components/About";
 
 const Web = () => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [formData, setFormData] = useState({});
 
   return (
     <div className="flex flex-col gap-5 w-60">
       <input
-        onChange={(e) => setName(e.target.value)}
+        onChange={(e) => setFormData({ ...formData, name: e.target.value })}
         className="border-2"
         type="text"
         placeholder="Name"
       />
 
       <input
-        onChange={(e) => setEmail(e.target.value)}
+        onChange={(e) => setFormData({ ...formData, email: e.target.value })}
         className="border-2"
         type="text"
         placeholder="Email"
       />
 
       <input
-        onChange={(e) => setPassword(e.target.value)}
+        onChange={(e) => setFormData({ ...formData, password: e.target.value })}
         className="border-2"
         type="password"
         placeholder="Password"
       />
 
-      <h1>This is Name - {name}</h1>
-      <h1>This is Name - {email}</h1>
-      <h1>This is Name - {password}</h1>
+      <h1>This is Name - {formData.name}</h1>
+      <h1>This is Email - {formData.email}</h1>
+      <h1>This is Password - {formData.password}</h1>
     </div>
   );
 };

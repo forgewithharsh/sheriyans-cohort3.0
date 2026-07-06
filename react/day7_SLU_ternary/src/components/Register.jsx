0b26414abe288278a12f372877c70844646b1f09
Harsh Guleria <guleriah59@gmail.com>
2026-07-06 15:00:41 +0530

    feat: implement user registration functionality with form handling

--- react/day7_SLU_ternary/src/components/Register.jsx
+++ react/day7_SLU_ternary/src/components/Register.jsx
@@ -2,29 +2,40 @@ import React, { useState } from "react";
 
 const Register = ({ setToggle }) => {
   const [formData, setFormData] = useState({});
+  const [users, setUsers] = useState([]);
+
+  console.log(users);
 
   const handleChange = (e) => {
     let { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setUsers([...users, formData]);
+  };
+
   return (
     <div className="bg-white w-90 p-6 rounded-xl flex flex-col gao-4">
       <h1>Register</h1>
-      <form action="" className="flex flex-col gap-4">
+      <form onSubmit={handleSubmit} action="" className="flex flex-col gap-4">
         <input
+          name="name"
           onChange={handleChange}
           className="p-2 border border-gray-400 rounded"
           type="text"
           placeholder="Name"
         />
         <input
+          name="email"
           onChange={handleChange}
           className="p-2 border border-gray-400 rounded"
           type="text"
           placeholder="Email"
         />
         <input
+          name="password"
           onChange={handleChange}
           className="p-2 border border-gray-400 rounded"
           type="password"

43f30d6eecc6b982c876925f7ef5b2c705760769
Harsh Guleria <guleriah59@gmail.com>
2026-07-09 08:40:21 +0530

    feat: integrate useForm for form handling and logging in RHF component

--- react/useRef_RHF/src/components/RHF.jsx
+++ react/useRef_RHF/src/components/RHF.jsx
@@ -1,28 +1,47 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
 const RHF = () => {
+  console.log("RHF Rendering!");
+
+  let {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
+
+  const formSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
     <div className="w-80 h-screen">
       <h1>REACT HOOK FORM</h1>
       <form
+        onSubmit={handleSubmit(formSubmit)}
         className="flex p-6 rounded bg-white flex-col gap-4"
       >
         <input
+          {...register("productName")}
           className="p-2 border border-gray-400 rounded"
           type="text"
           placeholder="Product name"
         />
         <input
+          {...register("price")}
           className="p-2 border border-gray-400 rounded"
           type="text"
           placeholder="Price"
         />
         <input
+          {...register("category")}
           className="p-2 border border-gray-400 rounded"
           type="text"
           placeholder="Category"
         />
         <input
+          {...register("image")}
           className="p-2 border border-gray-400 rounded"
           type="text"
           placeholder="Image"

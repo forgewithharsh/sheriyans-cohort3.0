19a9b8d6e3f4ff13c6f971df8b9dc9e7d2e891e6
Harsh Guleria <guleriah59@gmail.com>
2026-07-09 20:56:12 +0530

    implementing localStorage for users

---
+++ react/day9-LS_ContextApi/src/components/Form.jsx
@@ -0,0 +1,91 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+
+const Form = ({ setUsers, setToggle }) => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm({
+    mode: "onChange",
+  });
+
+  // console.log("errors", errors);
+
+  let formSubmit = (data) => {
+    console.log(data);
+    setUsers((prev) => [...prev, data]);
+    reset();
+    setToggle((prev) => !prev);
+  };
+
+  return (
+    <div className="flex flex-col items-center gap-3">
+      <h1 className="text-xl font-bold">Create User</h1>
+      <form
+        onSubmit={handleSubmit(formSubmit)}
+        className="w-90 flex flex-col bg-blue-200 gap-3 p-4 rounded border-2 border-black"
+      >
+        <input
+          {...register("name", {
+            required: "Name is required",
+            pattern: {
+              value: /^\S.*$/,
+              message: "Blank spaces are not allowed",
+            },
+          })}
+          className="p-2 outline-0 rounded border border-black"
+          type="text"
+          placeholder="Name"
+        />
+        {errors.name && <p className="text-red-500">{errors.name.message}</p>}
+        <input
+          {...register("email", {
+            required: "Email is required",
+            pattern: {
+              value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+              message: "Please enter valid email",
+            },
+          })}
+          className="p-2 outline-0 rounded border border-black"
+          type="email"
+          placeholder="Email"
+        />
+        {errors.email && <p className="text-red-500">{errors.email.message}</p>}
+        <input
+          {...register("mobile", {
+            required: "Mobile is required",
+            minLength: {
+              value: 10,
+              message: "Minimum 10 digits are required",
+            },
+            maxLength: {
+              value: 10,
+              message: "Maximum 10 digits are required",
+            },
+          })}
+          className="p-2 outline-0 rounded border border-black"
+          type="number"
+          placeholder="Mobile"
+        />
+        {errors.mobile && (
+          <p className="text-red-500">{errors.mobile.message}</p>
+        )}
+        <input
+          {...register("image", { required: "Image is required" })}
+          className="p-2 outline-0 rounded border border-black"
+          type="url"
+          placeholder="Image"
+        />
+        {errors.image && <p className="text-red-500">{errors.image.message}</p>}
+
+        <button className="text-white bg-blue-700 p-2 rounded-xl cursor-pointer">
+          Add User
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Form;

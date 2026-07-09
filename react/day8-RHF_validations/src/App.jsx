53096813feda6c3b63cdededb3c6dcdcea90673a
Harsh Guleria <guleriah59@gmail.com>
2026-07-09 15:01:13 +0530

    feat: update App and Navbar components with layout and styling enhancements

--- react/day8-RHF_validations/src/App.jsx
+++ react/day8-RHF_validations/src/App.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
+import Navbar from "./components/Navbar"
 
 const App = () => {
   return (
-    <div>
-
+    <div className="p-3 h-screen">
+      <Navbar />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- react/day8-RHF_validations/src/components/Navbar.jsx
+++ react/day8-RHF_validations/src/components/Navbar.jsx
@@ -2,8 +2,22 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div>
-      <h1>This is Navbar</h1>
+    <div className="p-4 bg-black text-white rounded flex items-center justify-between">
+      <div>
+        <img className="rounded-full"
+        width={40}
+          src="https://img.magnific.com/free-vector/blue-circle-with-white-user_78370-4707.jpg?semt=ais_hybrid&w=740&q=80"
+          alt=""
+        />
+      </div>
+      <div className="flex gap-6 font-semibold ">
+        <p>Home</p>
+        <p>About</p>
+        <p>Contact</p>
+      </div>
+      <button className="p-2 bg-blue-700 text-white cursor-pointer rounded">
+        Create User
+      </button>
     </div>
   );
 };

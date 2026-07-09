8e6ff52d483c18e2440ffc14c14216454a4d20cd
Harsh Guleria <guleriah59@gmail.com>
2026-07-09 07:48:37 +0530

    feat: refactor Form component to manage product state and streamline handleSubmit

--- react/useRef_RHF/src/components/Form.jsx
+++ react/useRef_RHF/src/components/Form.jsx
@@ -1,16 +1,23 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 
 const Form = () => {
   console.log("Form Rendering...");
+
+  const [products, setProducts] = useState({});
 
   const formRef = useRef({});
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(formRef.current.productName.value);
-    console.log(formRef.current.price.value);
-    console.log(formRef.current.category.value);
-    console.log(formRef.current.image.value);
+
+    let obj = {
+      pName: formRef.current.productName.value,
+      price: formRef.current.price.value,
+      category: formRef.current.category.value,
+      image: formRef.current.image.value,
+    };
+
+    setProducts(obj);
   };
 
   return (
@@ -26,25 +33,33 @@ const Form = () => {
           placeholder="Product name"
         />
         <input
-        ref={(e) => (formRef.current.price = e)}
+          ref={(e) => (formRef.current.price = e)}
           className="p-2 border border-gray-400 rounded"
           type="text"
           placeholder="Price"
         />
         <span>Select category:</span>
-        <select ref={(e) => (formRef.current.category = e)} className="p-2 border border-gray-400 rounded">
+        <select
+          ref={(e) => (formRef.current.category = e)}
+          className="p-2 border border-gray-400 rounded"
+        >
           <option value="mens">Mens</option>
           <option value="women">Women</option>
           <option value="kids">Kids</option>
         </select>
         <input
-        ref={(e) => (formRef.current.image = e)}
+          ref={(e) => (formRef.current.image = e)}
           className="p-2 border border-gray-400 rounded"
           type="text"
           placeholder="Image"
         />
         <button className="p-2 bg-blue-600 text-white rounded">CREATE</button>
       </form>
+
+      <h1>{products.pName}</h1>
+      <h1>{products.price}</h1>
+      <h1>{products.category}</h1>
+      <h1>{products.image}</h1>
     </div>
   );
 };

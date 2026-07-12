68e643e507ac3a7d9181f3375b872c6b5ebb0197
Harsh Guleria <guleriah59@gmail.com>
2026-07-12 12:08:12 +0530

    feat: implement ContextProvider for state management in main application

--- react/day10-content_Api/src/context/MyContext.jsx
+++ react/day10-content_Api/src/context/MyContext.jsx
@@ -1,3 +1,11 @@
 import { createContext } from "react";
 
+// setup out blank store
 const MyStore = createContext();
+
+// make a provider of our store who handle data
+// and serves to the customers
+
+export const ContextProvider = ({ children }) => {
+  return <MyStore.Provider>{children}</MyStore.Provider>;
+};

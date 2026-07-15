261e978b7d7b1c8e3a680e8e333fdb9c2c228704
Harsh Guleria <guleriah59@gmail.com>
2026-07-15 12:21:41 +0530

    feat: implement CartScreen component and integrate cart functionality in Navbar and App components

--- react/day12-project_with_api_call/src/App.jsx
+++ react/day12-project_with_api_call/src/App.jsx
@@ -2,10 +2,11 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Navbar from "./components/Navbar";
 import ProductCard from "./components/ProductCards";
+import CartScreen from "./pages/CartScreen";
 
 const App = () => {
   const [productsData, setProductsData] = useState([]);
-  console.log(productsData);
+  const [cartOpen, setCartOpen] = useState(false);
 
   async function getProductsData() {
     try {
@@ -23,13 +24,19 @@ const App = () => {
 
   return (
     <div className="h-screen p-2 flex flex-col gap-4">
-      <Navbar />
+      <Navbar setCartOpen={setCartOpen}/>
 
-      <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-        {productsData.map((elem) => {
-          return <ProductCard key={elem.id} product={elem} />;
-        })}
-      </div>
+      {cartOpen ? (
+        <div className="h-screen">
+          <CartScreen />
+        </div>
+      ) : (
+        <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+          {productsData.map((elem) => {
+            return <ProductCard key={elem.id} product={elem} />;
+          })}
+        </div>
+      )}
     </div>
   );
 };
--- react/day12-project_with_api_call/src/components/Navbar.jsx
+++ react/day12-project_with_api_call/src/components/Navbar.jsx
@@ -1,12 +1,26 @@
 import React from "react";
 
-const Navbar = () => {
+const Navbar = ({ setCartOpen }) => {
   return (
     <div className="bg-black text-white rounded p-5 flex items-center justify-between">
       <div>Logo</div>
       <div className="flex gap-10 text-xl">
-        <p className="cursor-pointer">Home</p>
-        <p className="cursor-pointer">Cart</p>
+        <p
+          onClick={() => {
+            setCartOpen(false);
+          }}
+          className="cursor-pointer"
+        >
+          Home
+        </p>
+        <p
+          onClick={() => {
+            setCartOpen(true);
+          }}
+          className="cursor-pointer"
+        >
+          Cart
+        </p>
       </div>
       <button>Login</button>
     </div>

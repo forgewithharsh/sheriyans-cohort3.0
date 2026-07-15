d708b29fdeeb7db3bd915cd4b5961f5213e583ed
Harsh Guleria <guleriah59@gmail.com>
2026-07-15 11:54:03 +0530

    feat: add Navbar and ProductCard components, implement product data fetching in App component

--- react/day12-project_with_api_call/src/App.jsx
+++ react/day12-project_with_api_call/src/App.jsx
@@ -1,22 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
+import Navbar from "./components/Navbar";
+import ProductCard from "./components/ProductCards";
 
 const App = () => {
-  async function getData() {
+  const [productsData, setProductsData] = useState([]);
+  console.log(productsData);
+
+  async function getProductsData() {
     try {
       let res = await axios.get("https://fakestoreapi.com/products");
 
-      console.log(res.data);
+      setProductsData(res.data);
     } catch (error) {
       console.log("Error: ", error.message);
     }
   }
 
-  getData();
+  useEffect(() => {
+    getProductsData();
+  }, []);
 
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="h-screen p-2 flex flex-col gap-4">
+      <Navbar />
+
+      <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        {productsData.map((elem) => {
+          return <ProductCard key={elem.id} product={elem} />;
+        })}
+      </div>
     </div>
   );
 };
---
+++ react/day12-project_with_api_call/src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="bg-black text-white rounded p-5 flex items-center justify-between">
+      <div>Logo</div>
+      <div className="flex gap-10 text-xl">
+        <p className="cursor-pointer">Home</p>
+        <p className="cursor-pointer">Cart</p>
+      </div>
+      <button>Login</button>
+    </div>
+  );
+};
+
+export default Navbar;

d49b2996cf06fbf721e785055fbc79377d7514f8
Harsh Guleria <guleriah59@gmail.com>
2026-07-15 11:37:47 +0530

    feat: add axios for API calls and implement data fetching in App component

--- react/day12-project_with_api_call/src/App.jsx
+++ react/day12-project_with_api_call/src/App.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
+import React from "react";
+import axios from "axios";
 
 const App = () => {
+  async function getData() {
+    try {
+      let res = await axios.get("https://fakestoreapi.com/products");
+
+      console.log(res.data);
+    } catch (error) {
+      console.log("Error: ", error.message);
+    }
+  }
+
+  getData();
+
   return (
     <div>
       <h1>Hello World</h1>
     </div>
-  )
-}
+  );
+};
 
-export default App
-
+export default App;

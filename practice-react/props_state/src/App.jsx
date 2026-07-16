6dbf5591644f6472695a6812e36683c5db8b292d
Harsh Guleria <guleriah59@gmail.com>
2026-07-17 05:04:05 +0530

    implementing counter functionality using useState hook

--- practice-react/props_state/src/App.jsx
+++ practice-react/props_state/src/App.jsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 
 const App = () => {
+  const [count, setCount] = useState(0);
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>Count is - {count}</h1>
+
+      <button onClick={() => setCount((prev) => prev + 1)}>Increment</button>
     </div>
   );
 };

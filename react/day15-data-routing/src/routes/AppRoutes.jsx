dcff104011256a4aeca84885ef93dc1f6b272023
Harsh Guleria <guleriah59@gmail.com>
2026-07-20 21:15:52 +0530

    Initialize day15-data-routing project with Vite, React, and Tailwind CSS setup
    - Set up main entry point in src/main.jsx to render AppRoutes
    - Created AppRoutes component for routing setup in src/routes/AppRoutes.jsx

---
+++ react/day15-data-routing/src/routes/AppRoutes.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { RouterProvider } from "react-router";
+
+const AppRoutes = () => {
+  let router = createBrowserRouter([]);
+
+  return <RouterProvider />;
+};
+
+export default AppRoutes;

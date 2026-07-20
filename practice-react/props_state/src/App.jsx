4b96f9453805313d4b478682f10e94ee5e67e9c3
Harsh Guleria <guleriah59@gmail.com>
2026-07-20 16:55:57 +0530

    feat: add Form component for product creation with input handling

--- practice-react/props_state/src/App.jsx
+++ practice-react/props_state/src/App.jsx
@@ -1,21 +1,25 @@
 import React from "react";
 import Login from "./components/Login";
-import { useState } from "react";
 import Register from "./components/Register";
 import UserCard from "./components/UserCard";
+import Form from "./components/Form";
 
 const App = () => {
-  const [users, setUsers] = useState([]);
+  // const [users, setUsers] = useState([]);
 
   return (
-    <div className="bg-gray-300 h-screen">
-      <Register setUsers={setUsers} />
+    // <div className="bg-gray-300 h-screen">
+    //   <Register setUsers={setUsers} />
 
-      <div className="flex gap-4">
-        {users.map((elem, index) => (
-          <UserCard key={index} user={elem} />
-        ))}
-      </div>
+    //   <div className="flex gap-4">
+    //     {users.map((elem, index) => (
+    //       <UserCard key={index} user={elem} />
+    //     ))}
+    //   </div>
+    // </div>
+
+    <div>
+      <Form/>
     </div>
   );
 };
---
+++ practice-react/props_state/src/components/Form.jsx
@@ -0,0 +1,63 @@
+import React, { useRef, useState } from "react";
+
+const Form = () => {
+  console.log("Form Rendering...");
+
+  const [products, setProducts] = useState({});
+
+  const formRef = useRef({});
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(formRef.current.productName.value);
+    console.log(formRef.current.price.value);
+    console.log(formRef.current.category.value);
+    console.log(formRef.current.image.value);
+  };
+
+  return (
+    <div className="w-80 h-screen">
+      <form
+        onSubmit={handleSubmit}
+        className="flex p-6 rounded bg-white flex-col gap-4"
+      >
+        <input
+          ref={(e) => (formRef.current.productName = e)}
+          className="p-2 border border-gray-400 rounded"
+          type="text"
+          placeholder="Product name"
+        />
+        <input
+          ref={(e) => (formRef.current.price = e)}
+          className="p-2 border border-gray-400 rounded"
+          type="text"
+          placeholder="Price"
+        />
+        <span>Select category:</span>
+        <select
+          ref={(e) => (formRef.current.category = e)}
+          className="p-2 border border-gray-400 rounded"
+        >
+          <option value="mens">Mens</option>
+          <option value="women">Women</option>
+          <option value="kids">Kids</option>
+        </select>
+        <input
+          ref={(e) => (formRef.current.image = e)}
+          className="p-2 border border-gray-400 rounded"
+          type="text"
+          placeholder="Image"
+        />
+        <button className="p-2 bg-blue-600 text-white rounded">CREATE</button>
+      </form>
+
+      <h1>{products.pName}</h1>
+      <h1>{products.price}</h1>
+      <h1>{products.category}</h1>
+      <h1>{products.image}</h1>
+    </div>
+  );
+};
+
+export default Form;

419f2f466654c4de0698843cabbcff2abf84743d
Harsh Guleria <guleriah59@gmail.com>
2026-07-22 12:16:58 +0530

    feat: add Home, About, and Services components with routing setup

--- react/day15-data-routing/src/routes/AppRoutes.jsx
+++ react/day15-data-routing/src/routes/AppRoutes.jsx
@@ -1,12 +1,26 @@
 import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router";
+import Home from "../pages/Home";
+import About from "../pages/About";
+import Services from "../pages/Services";
 
 const AppRoutes = () => {
   let router = createBrowserRouter([
-    
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "/about",
+      element: <About />,
+    },
+    {
+      path: "/services",
+      element: <Services />,
+    },
   ]);
 
-  return <RouterProvider router={}/>;
+  return <RouterProvider router={router} />;
 };
 
 export default AppRoutes;

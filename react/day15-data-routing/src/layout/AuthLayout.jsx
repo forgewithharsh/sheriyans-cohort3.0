6187611d171a2c713f192bab177188d66b8c28d6
Harsh Guleria <guleriah59@gmail.com>
2026-07-22 15:30:03 +0530

    feat: implement MainLayout and AuthLayout components with routing structure

---
+++ react/day15-data-routing/src/layout/AuthLayout.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AuthLayout = () => {
+  return (
+    <div>
+      <h1>This is Auth Layout</h1>
+    </div>
+  );
+};
+
+export default AuthLayout;
--- react/day15-data-routing/src/routes/AppRoutes.jsx
+++ react/day15-data-routing/src/routes/AppRoutes.jsx
@@ -3,21 +3,33 @@ import { createBrowserRouter, RouterProvider } from "react-router";
 import Home from "../pages/Home";
 import About from "../pages/About";
 import Services from "../pages/Services";
+import MainLayout from "../layout/MainLayout";
+import AuthLayout from "../layout/AuthLayout";
 
 const AppRoutes = () => {
   let router = createBrowserRouter([
     {
       path: "/",
-      element: <Home />,
+      element: <MainLayout />,
+      children: [
+        {
+          path: "",
+          element: <Home />,
+        },
+        {
+          path: "about",
+          element: <About />,
+        },
+        {
+          path: "services",
+          element: <Services />,
+        },
+      ],
     },
     {
-      path: "/about",
-      element: <About />,
-    },
-    {
-      path: "/services",
-      element: <Services />,
-    },
+      path: "/auth",
+      element: <AuthLayout/>
+    }
   ]);
 
   return <RouterProvider router={router} />;

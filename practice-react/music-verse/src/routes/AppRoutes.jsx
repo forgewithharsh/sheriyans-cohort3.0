7578ebb16c265c281a06c7b34872c790866bb8b8
Harsh Guleria <guleriah59@gmail.com>
2026-07-24 11:09:57 +0530

    feat: define application routes with MainLayout and add Home, Favourite, Login, and Register pages

--- practice-react/music-verse/src/routes/AppRoutes.jsx
+++ practice-react/music-verse/src/routes/AppRoutes.jsx
@@ -1,12 +1,34 @@
 import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router";
 import MainLayout from "../layouts/MainLayout";
+import Home from "../pages/Home";
+import Favourite from "../pages/Favourite";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
 
 const AppRoutes = () => {
   const router = createBrowserRouter([
     {
       path: "/",
       element: <MainLayout />,
+      children: [
+        {
+          index: true,
+          element: <Home />,
+        },
+        {
+          path: "favourite",
+          element: <Favourite />,
+        },
+        {
+          path: "login",
+          element: <Login />,
+        },
+        {
+          path: "register",
+          element: <Register />,
+        },
+      ],
     },
   ]);
 

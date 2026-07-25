357cf7cb4135f054c606aee092c4115fbe17f166
Harsh Guleria <guleriah59@gmail.com>
2026-07-25 13:47:58 +0530

    feat: add memoization for calculation in App component

--- react/day17-memoization/src/App.jsx
+++ react/day17-memoization/src/App.jsx
@@ -2,11 +2,23 @@ import React, { useState } from "react";
 import Home from "./components/Home";
 import About from "./components/About";
 import { useCallback } from "react";
+import { useMemo } from "react";
 
 const App = () => {
   const [count, setCount] = useState(0);
   const [users, setUsers] = useState({ name: "Harsh", id: 789 });
   console.log("app rendering");
+
+  let calculation = useMemo(() => {
+    console.log("calculation running");
+
+    let sum = 0;
+
+    for (let i = 0; i < 100; i++) {
+      sum += i;
+    }
+    return sum;
+  }, []);
 
   let greet = useCallback(() => {
     console.log("hello...");
@@ -17,6 +29,8 @@ const App = () => {
       <h1>Memoization</h1>
       <h2>Count - {count}</h2>
       <button onClick={() => setCount(count + 1)}>Increment</button>
+
+      <h2>Calculations - {calculation}</h2>
 
       <h2>Name is {users.name}</h2>
       <button

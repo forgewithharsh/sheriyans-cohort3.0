e8ed442a46fafd13bcd324e2ea09cb1d6d51910c
Harsh Guleria <guleriah59@gmail.com>
2026-07-30 23:33:02 +0530

    feat: implement authentication flow with login and registration pages, add auth slice and hooks, and integrate toast notifications

---
+++ react/day20-authentication_redux/src/hooks/useAuth.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router";
+import { toast } from "react-toastify";
+import { addUser } from "../features/authSlice";
+
+const useAuth = () => {
+  const dispatch = useDispatch();
+
+  const [registeredUsers, setRegisteredUsers] = useState(
+    JSON.parse(localStorage.getItem("registeredUsers")) || [],
+  );
+
+  const navigate = useNavigate();
+
+  const {
+    register,
+    reset,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  let registerFormSubmit = (data) => {
+    let arr = [...registeredUsers, data];
+
+    setRegisteredUsers(arr);
+
+    localStorage.setItem("registeredUsers", JSON.stringify(arr));
+
+    toast.success("Registered Successfully!");
+    reset();
+  };
+
+  let loginFormSubmit = (data) => {
+    let user = registeredUsers.find((val) => {
+      return val.email === data.email || val.password === data.password;
+    });
+
+    if (!user) {
+      toast.error("Invalid Something...");
+      return;
+    }
+
+    dispatch(addUser(user));
+    localStorage.setItem("loggedInUser", JSON.stringify(user))
+    toast.success("User Logged In...");
+
+    reset();
+  };
+
+  return {
+    navigate,
+    register,
+    handleSubmit,
+    errors,
+    loginFormSubmit,
+    registerFormSubmit,
+  };
+};
+
+export default useAuth;
--- react/day20-authentication_redux/src/routes/AppRoutes.jsx
+++ react/day20-authentication_redux/src/routes/AppRoutes.jsx
@@ -1,12 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { createBrowserRouter, RouterProvider } from "react-router";
 import AppLayout from "../layouts/AppLayout";
 import LoginPage from "../pages/LoginPage";
 import RegisterPage from "../pages/RegisterPage";
 import MainLayout from "../layouts/MainLayout";
 import HomePage from "../pages/HomePage";
+import { toast } from "react-toastify";
+import { useDispatch } from "react-redux";
+import { addUser } from "../features/authSlice";
 
 const AppRoutes = () => {
+  const dispatch = useDispatch();
+
+  const hydrateUser = () => {
+    let loggedInUser = JSON.parse(localStorage.getItem("loggedInUser"));
+
+    if (!loggedInUser) {
+      toast.error("Unauthorized User");
+      return;
+    }
+
+    dispatch(addUser(loggedInUser));
+  };
+
+  useEffect(() => {
+    hydrateUser();
+  }, []);
+
   const router = createBrowserRouter([
     {
       path: "/",

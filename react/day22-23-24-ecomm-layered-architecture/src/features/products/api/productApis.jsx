6272525df6607ed2db2b405353f20a54da86b106
Harsh Guleria <guleriah59@gmail.com>
2026-08-06 01:07:12 +0530

    feat: enhance Navbar with active link styles and add cart/orders navigation

---
+++ react/day22-23-24-ecomm-layered-architecture/src/features/products/api/productApis.jsx
@@ -0,0 +1,6 @@
+const getAllProductsApi = async () => {
+  try {
+  } catch (error) {
+    console.log("Error in getting all products api", error);
+  }
+};
--- react/day22-23-24-ecomm-layered-architecture/src/shared/components/Navbar.jsx
+++ react/day22-23-24-ecomm-layered-architecture/src/shared/components/Navbar.jsx
@@ -4,18 +4,54 @@ import { NavLink } from "react-router";
 
 const Navbar = () => {
   return (
-    <div className="flex items-center gap-5 justify-between py-4 px-10">
+    <div className="flex items-center gap-5 justify-between bg-yellow-600 py-4 px-10">
       <h1>Logo</h1>
 
       <div className="flex items-center gap-10 text-xl">
-        <NavLink to={"/main"}>Home</NavLink>
-        <NavLink to={"/main/product"}>Shop</NavLink>
-        <NavLink to={"/main/about"}>About</NavLink>
+        <NavLink
+          className={({ isActive }) =>
+            isActive ? "text-white font-semibold" : "text-gray-200"
+          }
+          to={"/main"}
+          end
+        >
+          Home
+        </NavLink>
+        <NavLink
+          className={({ isActive }) =>
+            isActive ? "text-white font-semibold" : "text-gray-200"
+          }
+          to={"/main/product"}
+        >
+          Shop
+        </NavLink>
+        <NavLink
+          className={({ isActive }) =>
+            isActive ? "text-white font-semibold" : "text-gray-200"
+          }
+          to={"/main/about"}
+        >
+          About
+        </NavLink>
       </div>
 
       <div className="flex items-center gap-6">
-        <ShoppingCart />
-        <Box />
+        <NavLink
+          className={({ isActive }) =>
+            isActive ? "text-white font-semibold" : "text-gray-200"
+          }
+          to={"/main/cart"}
+        >
+          <ShoppingCart />{" "}
+        </NavLink>
+        <NavLink
+          className={({ isActive }) =>
+            isActive ? "text-white font-semibold" : "text-gray-200"
+          }
+          to={"/main/orders"}
+        >
+          <Box />
+        </NavLink>
         <button className="px-5 py-1 rounded cursor-pointer text-white bg-red-700">
           Logout
         </button>

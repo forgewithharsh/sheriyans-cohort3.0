f056eb43d23fd8cf4cea3b5e3409dd13f70b41c9
Harsh Guleria <guleriah59@gmail.com>
2026-08-09 14:32:46 +0530

    feat: implement Infinite component for product listing with infinite scroll and loading state

--- react/day25-pagination_infinite_scroll/src/api/productApi.jsx
+++ react/day25-pagination_infinite_scroll/src/api/productApi.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-export const getAllProducts = async (limit, page = 1) => {
+export const getAllProducts = async (limit, pageParams = 1) => {
   try {
-    const res = await axios.get(`https://dummyjson.com/products?limit=${limit}&skip=${page * limit}`);
+    const res = await axios.get(`https://dummyjson.com/products?limit=${limit}&skip=${pageParams}`);
     return res.data;
   } catch (error) {
     console.log("Error in api", error);

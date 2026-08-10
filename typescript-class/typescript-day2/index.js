1be44b1038b8cc35001da9e13b75d4c1fc1cedc3
Harsh Guleria <guleriah59@gmail.com>
2026-08-10 21:09:28 +0530

    Add package.json and tsconfig.json for TypeScript Day 2 setup
    - Created package.json with basic configuration and dev dependencies including tsx and typescript.
    - Added tsconfig.json with compiler options for TypeScript, enabling strict type checking and module settings.

---
+++ typescript-class/typescript-day2/index.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// Annotation
+let a = 90;
+console.log(a);
+// Inference
+let b = "pol";
+// b = 90;
+console.log(a);
+//# sourceMappingURL=index.js.map
---
+++ typescript-class/typescript-day2/index.ts
@@ -0,0 +1,9 @@
+// Annotation
+let a:number = 90;
+console.log(a)
+
+// Inference
+let b = "pol";
+// b = 90;
+
+console.log(a)

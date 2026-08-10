db3e06cd819fb8fe76c071ffed72b0fd054ea41e
Harsh Guleria <guleriah59@gmail.com>
2026-08-10 21:31:53 +0530

    feat: add array and tuple examples to TypeScript file

--- typescript-class/typescript-day2/index.js
+++ typescript-class/typescript-day2/index.js
@@ -7,4 +7,14 @@ console.log(a);
 let b = "pol";
 // b = 90;
 console.log(a);
+// Arrays
+let arr = [5, 6, 7, 8, 8, 9, 0, "hello", "jio", true, null, undefined];
+let arr1 = ["jio", "pol", "join"];
+// Tuples
+let arr2 = [57, 87];
+let data = [
+    { name: "pol" },
+    { name: "pol" },
+    { name: "pol" },
+];
 //# sourceMappingURL=index.js.map
--- typescript-class/typescript-day2/index.ts
+++ typescript-class/typescript-day2/index.ts
@@ -1,9 +1,24 @@
 // Annotation
-let a:number = 90;
-console.log(a)
+let a: number = 90;
+console.log(a);
 
 // Inference
 let b = "pol";
 // b = 90;
 
-console.log(a)
+console.log(a);
+
+// Arrays
+let arr: any[] = [5, 6, 7, 8, 8, 9, 0, "hello", "jio", true, null, undefined];
+
+let arr1: string[] = ["jio", "pol", "join"];
+
+// Tuples
+
+let arr2: [number, number] = [57, 87];
+
+let data: [{ name: string }, { name: string }, { name: string }] = [
+  { name: "pol" },
+  { name: "pol" },
+  { name: "pol" },
+];

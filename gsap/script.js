15eee09edc77e55abb9870cc407023f2adf29e1d
Harsh Guleria <guleriah59@gmail.com>
2026-08-11 22:11:59 +0530

    feat: update GSAP animation and styling for multiple boxes

--- gsap/script.js
+++ gsap/script.js
@@ -1,5 +1,20 @@
-gsap.to("#box", {
-  x:1000,
-  duration:2,
-  delay:1
-})
+gsap.to("#box1", {
+  x: 1500,
+  duration: 2,
+  delay: 1,
+  rotate: 360,
+});
+
+gsap.to("#box2", {
+  x: 1500,
+  duration: 1.5,
+  delay: 2.5,
+});
+
+gsap.to("#box3", {
+  x: 1500,
+  scale: 0.5,
+  borderRadius: "50%",
+  duration: 1.5,
+  delay: 4,
+});

5c3813829f830768703e5bfb89b6fb775ac3f034
Harsh Guleria <guleriah59@gmail.com>
2026-08-11 22:00:28 +0530

    Add package.json and tsconfig.json for TypeScript project setup
    - Created package.json with basic configuration and dev dependencies for TypeScript and tsx.
    - Added tsconfig.json with compiler options for Node.js environment and strict type checking.

---
+++ typescript-class/typescript-day3/index.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let userObj = {
+    name: "rahul",
+    age: 54,
+    company: "sheryians",
+    address: {
+        street: "Malabar road",
+        city: "Mumbai",
+        state: "Maharastra",
+    },
+};
+userObj.name = 90;
+//# sourceMappingURL=index.js.map
---
+++ typescript-class/typescript-day3/index.ts
@@ -0,0 +1,12 @@
+let userObj = {
+  name: "rahul",
+  age: 54,
+  company: "sheryians",
+  address: {
+    street: "Malabar road",
+    city: "Mumbai",
+    state: "Maharastra",
+  },
+};
+
+userObj.name = 90;

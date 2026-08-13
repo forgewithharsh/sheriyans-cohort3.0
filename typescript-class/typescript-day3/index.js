722c7acc3b3caddee1ebe7bf0c4cab9a28446a13
Harsh Guleria <guleriah59@gmail.com>
2026-08-13 19:18:45 +0530

    feat: define UserObj type and refactor userObj initialization; implement sum, mul, and add functions

--- typescript-class/typescript-day3/index.js
+++ typescript-class/typescript-day3/index.js
@@ -10,5 +10,23 @@ let userObj = {
         state: "Maharastra",
     },
 };
-userObj.name = 90;
+userObj.employeeId = "har123";
+// console.log(userObj);
+let sum = (a, b) => {
+    return a + b;
+};
+// console.log(sum(20, 20));
+let mul = (a, b) => {
+    let data = b();
+    return a * data;
+};
+let res = mul(20, () => 10);
+// console.log(res);
+let add = (a) => (b) => {
+    if (b !== undefined)
+        return add(a + b);
+    return a;
+};
+let data = add(90)(10)(90)();
+console.log(data);
 //# sourceMappingURL=index.js.map
--- typescript-class/typescript-day3/index.ts
+++ typescript-class/typescript-day3/index.ts
@@ -1,4 +1,16 @@
-let userObj = {
+type UserObj = {
+  name: string;
+  age: number;
+  company: string;
+  address: {
+    street: string;
+    city: string;
+    state: string;
+  };
+  employeeId?: string;
+};
+
+let userObj: UserObj = {
   name: "rahul",
   age: 54,
   company: "sheryians",
@@ -9,4 +21,28 @@ let userObj = {
   },
 };
 
-userObj.name = 90;
+userObj.employeeId = "har123";
+
+// console.log(userObj);
+
+let sum = (a: number, b: number): number => {
+  return a + b;
+};
+
+// console.log(sum(20, 20));
+
+let mul = (a: number, b: () => number): number => {
+  let data = b();
+  return a * data;
+};
+
+let res = mul(20, () => 10);
+// console.log(res);
+
+let add = (a: number) => (b: number) => {
+  if (b !== undefined) return add(a + b);
+  return a;
+};
+
+let data = add(90)(10)(90)();
+console.log(data);

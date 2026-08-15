266cf116a540725a50137e89e73e825aa3217430
Harsh Guleria <guleriah59@gmail.com>
2026-08-15 23:58:37 +0530

    feat: update loginFormSubmit to dispatch loginUserAction for user authentication

--- practice-react/skymart_project/src/hooks/useAuth.jsx
+++ practice-react/skymart_project/src/hooks/useAuth.jsx
@@ -40,7 +40,7 @@ export const useAuth = () => {
   const loginFormSubmit = (data) => {
     try {
       // Api Call
-      console.log(data);
+      dispatch(loginUserAction(data));
     } catch (error) {
       console.log("Form api error", error);
     }

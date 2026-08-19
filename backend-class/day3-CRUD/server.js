ef0f17bc8abfd607de39de7d4e3cd8aaa3b85d6d
Harsh Guleria <guleriah59@gmail.com>
2026-08-19 15:02:44 +0530

    Add initial setup for day3-CRUD project with Express
    - Created package.json with Express as a dependency
    - Implemented basic server setup in server.js
    - Configured server to listen on port 3000

---
+++ backend-class/day3-CRUD/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+
+app.post((req, res) => {
+  res.send;
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});

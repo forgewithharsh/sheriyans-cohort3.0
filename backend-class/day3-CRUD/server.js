6f4eede0739a3e8ba55350a59a900df4c2bb90da
Harsh Guleria <guleriah59@gmail.com>
2026-08-19 17:54:14 +0530

    fix: correct POST endpoint definition and implement user creation logic

--- backend-class/day3-CRUD/server.js
+++ backend-class/day3-CRUD/server.js
@@ -3,8 +3,20 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
-app.post((req, res) => {
-  res.send;
+const users = [];
+
+// post - create
+app.post("/create", (req, res) => {
+  const body = req.body;
+
+  users.push(body);
+
+  res.send(users);
+});
+
+// get - read
+app.get((req, res) => {
+  res.send("ok got it");
 });
 
 const port = 3000;

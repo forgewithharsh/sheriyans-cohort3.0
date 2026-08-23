5fc51defce2bf3b37a7585721f50605af067a0d6
Harsh Guleria <guleriah59@gmail.com>
2026-08-23 11:32:52 +0530

    feat: implement note creation endpoint with success response

--- backend-class/day4-dbconnection/src/app.js
+++ backend-class/day4-dbconnection/src/app.js
@@ -1,12 +1,29 @@
 const express = require("express");
 const connectDb = require("./config/db.js");
+const NotesModel = require("./models/notes.model.js");
 
 const app = express();
+app.use(express.json());
 
 connectDb();
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.post("/create", async(req, res) => {
+  let { title, description } = req.body;
+
+  let newNote = await NotesModel.create({
+    title,
+    description,
+  });
+
+  res.send({
+    success: true,
+    meassage: "Note created successfully",
+    data: newNote,
+  });
 });
 
 module.exports = app;

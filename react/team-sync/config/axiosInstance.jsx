612c80630dfdd3f9cda923f07ec6b3a5ef2456eb
Harsh Guleria <guleriah59@gmail.com>
2026-08-30 18:56:53 +0530

    feat: add response interceptor to handle 401 errors and refresh access token

--- react/team-sync/config/axiosInstance.jsx
+++ react/team-sync/config/axiosInstance.jsx
@@ -4,3 +4,21 @@ export const axiosInstance = axios.create({
   baseURL: "https://api.team-sync.space/api",
   withCredentials: true,
 });
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    let originalReq = error.config;
+    if (error.response.status === 401 && !originalReq._retry) {
+      originalReq._retry = true;
+
+      try {
+        await axiosInstance.get("/auth./get-accessToken")
+        return axiosInstance(originalReq)
+      } catch (error) {
+        window.location.href("/");
+        return Promise.reject(error);
+      }
+    }
+  },
+);

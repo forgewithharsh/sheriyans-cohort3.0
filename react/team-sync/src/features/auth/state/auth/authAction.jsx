314ccf8ce8696ea6242df6397b6ca3ca7d237561
Harsh Guleria <guleriah59@gmail.com>
2026-08-30 09:19:48 +0530

    feat: add axios instance and loginEmployee action for authentication

---
+++ react/team-sync/src/features/auth/state/auth/authAction.jsx
@@ -0,0 +1,14 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { axiosInstance } from "../../../../../config/axiosInstance";
+
+export const loginEmployee = createAsyncThunk(
+  "auth/login",
+  async (credentials, thunkApi) => {
+    try {
+      let res = await axiosInstance.post("/auth/login", credentials);
+      return res.data;
+    } catch (error) {
+      return thunkApi.rejectWithValue(error);
+    }
+  },
+);

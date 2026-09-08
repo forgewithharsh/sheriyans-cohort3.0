128615ec6989aec8fbad71191b8e8ce6bf325486
Harsh Guleria <guleriah59@gmail.com>
2026-09-08 21:12:40 +0530

    feat: implement user registration endpoint with validation and error handling

--- pure_backend/03-access-refresh-token/src/routes/auth.routes.js
+++ pure_backend/03-access-refresh-token/src/routes/auth.routes.js
@@ -1,9 +1,29 @@
-import {Router} from "express"
-import app from "../app/app";
+import { Router } from "express";
+import userModel from "../model/user.model.js";
+import bcrypt from "bcryptjs"
 
-const router = Router()
+const router = Router();
 
+router.post("/register", async (req, res) => {
+  const { name, email, password } = req.body;
 
+  const isUserExists = await userModel.findOne({ email });
 
+  if (isUserExists) {
+    return res.status(400).json({
+      message: "User already exists",
+      errors: [
+        {
+          path: "email",
+          message: "User already exists",
+        },
+      ],
+    });
+  }
 
-export default app;
+  const user = await userModel.create({
+    name, email, passwordHash: await bcrypt.hash(password, 12)
+  })
+});
+
+export default router;

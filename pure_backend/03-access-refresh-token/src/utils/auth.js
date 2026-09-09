ce65f711e9c7e0ce3d2fa3f5a30aa83240b15372
Harsh Guleria <guleriah59@gmail.com>
2026-09-09 20:48:31 +0530

    feat: add token generation utility for access and refresh tokens

---
+++ pure_backend/03-access-refresh-token/src/utils/auth.js
@@ -0,0 +1,16 @@
+import jwt from "jsonwebtoken";
+
+import config from "../config/config.js";
+
+export const generateTokens = ({ userd }) => {
+  const accessToken = jwt.sign({ id: userd._id }, config.ACCESS_TOKEN_SECRET, {
+    expiresIn: "15m",
+  });
+  const refreshToken = jwt.sign(
+    { id: userd._id },
+    config.REFRESH_TOKEN_SECRET,
+    { expiresIn: "7d" },
+  );
+
+  return { accessToken, refreshToken };
+};

efd2bac3f533d2ce03201411d9d83a46751e0dbd
Harsh Guleria <guleriah59@gmail.com>
2026-09-11 10:29:35 +0530

    refactor: improve error handling and response structure in auth routes

--- pure_backend/03-access-refresh-token/src/routes/auth.routes.js
+++ pure_backend/03-access-refresh-token/src/routes/auth.routes.js
@@ -18,7 +18,12 @@ router.post("/register", async (req, res) => {
   if (isUserExists) {
     return res.status(400).json({
       message: "User already exists",
-      errors: [{ path: "email", message: "User already exists" }],
+      errors: [
+        {
+          path: "email",
+          message: "User already exists",
+        },
+      ],
     });
   }
 
@@ -37,87 +42,40 @@ router.post("/register", async (req, res) => {
     httpOnly: true,
   });
 
-  res.status(201).json({
+  return res.status(201).json({
     message: "User registered successfully",
     data: {
-      user: {
-        name: user.name,
-        email: user.email,
-      },
+      name: user.name,
+      email: user.email,
     },
     accessToken,
   });
 });
 
-// *@GET /api/auth/me
+// *GET /api/auth/me
 router.get("/me", async (req, res) => {
   const accessToken = req.headers.authorization?.split(" ")[1];
 
   if (!accessToken) {
-    return res.status(401).json({
-      message: "Unauthorized, access token not found",
+    return res.status(400).json({
+      message: "Unauthorized, Invalid or expired access token",
     });
   }
 
   try {
     const decoded = verifyAccessToken(accessToken);
 
+    console.log(decoded);
+
     const user = await userModel.findById(decoded.id);
 
-    res.status(200).json({
-      message: "User fetched successfully",
+    return res.status(200).json({
       data: {
         user: {
           name: user.name,
           email: user.email,
         },
       },
-    });
-  } catch (error) {
-    return res.status(401).json({
-      message: "Unauthorized, Invalid or expired access token",
-    });
-  }
-});
-
-// *@POST /api/auth/refresh
-router.post("/refresh", async (req, res) => {
-  const refreshToken = req.cookies.refreshToken;
-
-  if (!refreshToken) {
-    return res.status(401).json({
-      message: "Unauthorized, refresh token not found",
-    });
-  }
-
-  try {
-    const decoded = verifyRefreshToken(refreshToken);
-
-    const user = await userModel.findById(decoded.id);
-
-    if (refreshToken !== user.refreshToken) {
-      user.refreshToken = null;
-      await user.save();
-
-      return res.status(401).json({
-        message: "Unauthorized, refresh token mismatch",
-      });
-    }
-
-    const { accessToken, refreshToken: newRefreshToken } = generateTokens({
-      userId: user._id,
-    });
-
-    res.cookie("refreshToken", newRefreshToken, {
-      httpOnly: true,
-    });
-
-    user.refreshToken = newRefreshToken;
-    await user.save();
-
-    return res.status(200).json({
-      message: "Tokens refreshed successfully",
-      accessToken,
     });
   } catch (error) {
     return res.status(401).json({

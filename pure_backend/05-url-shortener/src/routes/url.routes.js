35a39fe79eb849b3cc78108c6adf16bc2d976c71
Harsh Guleria <guleriah59@gmail.com>
2026-09-13 12:37:10 +0530

    feat: implement URL shortener backend
    - add package.json for project dependencies and scripts
    - create app.js to set up Express server and routes
    - add config.js for environment variable management
    - implement db.js for MongoDB connection
    - define user.model.js for URL schema
    - create url.routes.js for handling URL shortening logic
    - add server.js to start the application
    - implement generateCode.js for generating short codes

---
+++ pure_backend/05-url-shortener/src/routes/url.routes.js
@@ -0,0 +1,60 @@
+import express from "express";
+import generateCode from "../utils/generateCode.js";
+import urlModel from "../model/user.model.js";
+
+let router = express.Router();
+
+// * @POST /api/url
+router.post("/", async function (req, res) {
+  let { url } = req.body;
+
+  if (!url) {
+    return res.status(400).json({
+      error: "Please enter a URL",
+    });
+  }
+
+  if (
+    url.startsWith("http://") == false &&
+    url.startsWith("https://") == false
+  ) {
+    return res.status(400).json({
+      error: "Please enter a valid URL starting with http:// and https://",
+    });
+  }
+
+  if (url.length > 2048) {
+    return res.status(400).json({
+      error: "URL is too long.",
+    });
+  }
+
+  let code = generateCode();
+
+  let newUrl = await urlModel.create({
+    originalUrl: url,
+    shortCode: code,
+  });
+
+  return res.status(201).json({
+    message: "URL shortened successfully",
+    data: {
+      originalUrl: newUrl.originalUrl,
+      shortCode: newUrl.shortCode,
+    },
+  });
+});
+
+// * @GET /api/url
+router.get("/", async function (req, res) {
+  const urls = await urlModel.find();
+
+  return res.status(200).json({
+    message: "URLs fetched successfully",
+    data: {
+      urls,
+    },
+  });
+});
+
+export default router;
--- pure_backend/05-url-shortener/src/utils/generateCode.js
+++ pure_backend/05-url-shortener/src/utils/generateCode.js
@@ -1,14 +1,14 @@
-const generateCode = () => {
-  const mainString =
+let generateCode = () => {
+  let mainString =
     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-  const shortCode = "";
+  let shortCode = "";
 
   for (let i = 0; i < 6; i++) {
     shortCode += mainString.charAt(Math.floor(Math.random() * 62));
   }
 
-  return shortCode
+  return shortCode;
 };
 
 export default generateCode;

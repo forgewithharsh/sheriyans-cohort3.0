4f8f0ce98dc9992c63df5ab9020baa4dcba98a2d
Harsh Guleria <guleriah59@gmail.com>
2026-09-18 14:37:07 +0530

    feat: add express-validator dependency for input validation

--- pure_backend/06-validation/src/routes/auth.routes.js
+++ pure_backend/06-validation/src/routes/auth.routes.js
@@ -1,9 +1,44 @@
 import { Router } from "express";
+import { body, validationResult } from "express-validator";
 import { register } from "../controller/auth.controller.js";
 
 const router = Router();
 
 // * @POST /api/auth/register
-router.post("/register", register);
+router.post(
+  "/register",
+  [
+    body("email")
+      .exists()
+      .withMessage("Email is Required")
+      .isEmail()
+      .withMessage("Invalid Email Address"),
+    body("phone")
+      .exists()
+      .withMessage("Phone Number is Required")
+      .isMobilePhone("en-IN")
+      .withMessage("Invalid Phone Number"),
+    body("password")
+      .exists()
+      .withMessage("Password is Required")
+      .trim()
+      .isLength({ min: 6 })
+      .withMessage("Password at least 6 characters long"),
+
+    (req, res, next) => {
+      const errors = validationResult(req);
+
+      if (!errors.isEmpty()) {
+        return res.status(400).json({
+          message: "Invalid Request",
+          errors: errors.array(),
+        });
+      }
+
+      next();
+    },
+  ],
+  register,
+);
 
 export default router;

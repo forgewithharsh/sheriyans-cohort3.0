30304c05758a2d98ceba59d9e8f5f074149c7cec
Harsh Guleria <guleriah59@gmail.com>
2026-09-18 13:25:53 +0530

    feat: initialize 06-validation backend with Express and MongoDB setup
    - Add package.json with dependencies and devDependencies
    - Create app.js to set up Express server
    - Implement config.js for environment variables
    - Add db.js for MongoDB connection logic
    - Define user model in user.model.js with email, phone, and passwordHash fields
    - Set up server.js to connect to the database and start the server

--- pure_backend/04-frontend-integration/client/src/shared/api.js
+++ pure_backend/04-frontend-integration/client/src/shared/api.js
@@ -8,19 +8,6 @@ const useApi = () => {
     baseURL: "http://localhost:5173/api",
     withCredentials: true,
   });
-
-  api.interceptors.request.use(
-    (config) => {
-      if (accessToken) {
-        config.headers.Authorization = `Bearer ${accessToken}`;
-      }
-      return config;
-    },
-    (error) => {
-      return Promise.reject(error);
-    },
-  );
-  return api;
 };
 
 export default useApi;
---
+++ pure_backend/06-validation/src/app/app.js
@@ -0,0 +1,6 @@
+import express from "express";
+
+const app = expree();
+app.use(express.json());
+
+export default app;

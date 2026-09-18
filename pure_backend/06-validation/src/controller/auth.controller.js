3b41a4003237fb395613d2563a152a0fabbb1ae6
Harsh Guleria <guleriah59@gmail.com>
2026-09-18 14:42:52 +0530

    feat: implement user registration logic in the auth controller

--- pure_backend/06-validation/src/controller/auth.controller.js
+++ pure_backend/06-validation/src/controller/auth.controller.js
@@ -1,3 +1,18 @@
 export async function register(req, res) {
   const { email, phone, password } = req.body;
+
+  const user = await userModel.create({
+    email,
+    phone,
+    password,
+  });
+
+  res.status(201).json({
+    message: "User registered successfully",
+    data: {
+      email,
+      phone,
+      id: user._id,
+    },
+  });
 }

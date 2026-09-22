11c5f176d15193b8ba1fddeb3f8291e025227a19
Harsh Guleria <guleriah59@gmail.com>
2026-09-22 21:21:31 +0530

    feat: enhance product validation to include sizes and stock fields

--- pure_backend/07-snitch/server/src/validators/product.validator.js
+++ pure_backend/07-snitch/server/src/validators/product.validator.js
@@ -1,4 +1,4 @@
-import { body } from "express-validator";
+import { body, validationResult } from "express-validator";
 
 export const createProductValidator = [
   body("title")
@@ -11,8 +11,11 @@ export const createProductValidator = [
     .trim()
     .isLength({ min: 2, max: 100 })
     .withMessage("Title length must be between 2 to 100 charcters")
-    .isAlpha("en-US", { ignore: " " })
-    .withMessage("Title can only have english small case and capital case"),
+    .bail()
+    .isAlpha("en-US", { ignore: " -" })
+    .withMessage(
+      "Title can only have english small case and capital case character",
+    ),
   body("description")
     .exists()
     .withMessage("Description is required")
@@ -29,7 +32,9 @@ export const createProductValidator = [
     .withMessage("price amount is required")
     .bail()
     .isFloat({ min: 0 })
-    .withMessage("price amount must be a floating number")
+    .withMessage(
+      "price amount must be a floating number and must be greater than 0",
+    )
     .bail(),
   body("price.currency")
     .exists()
@@ -40,4 +45,41 @@ export const createProductValidator = [
     .bail()
     .isIn(["INR", "USD"])
     .withMessage("Currency either be INR or USD"),
+  body("sizes")
+    .exists()
+    .withMessage("Sizes are required")
+    .bail()
+    .isArray()
+    .withMessage("Sizes must be array of object"),
+  body("sizes.*.size")
+    .exists()
+    .withMessage("Sizes must be present in every entry of sizes array")
+    .bail()
+    .isString()
+    .withMessage("Size must be a string value")
+    .bail()
+    .isIn("XS", "S", "M", "L", "XL", "XXL")
+    .withMessage("Size can be one of these XS, S, M, L, XL, XXL"),
+  body("sizes.*.stock")
+    .exists()
+    .withMessage("Stock must be present in evry entry of the sizes array")
+    .bail()
+    .isInt({ min: 0 })
+    .withMessage("Stock must be a integer value")
+    .bail(),
+
+  (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        message: "Invalid Request",
+        errors: errors.array(),
+      });
+    }
+
+    next();
+  },
 ];
+
+export default createProductValidator;

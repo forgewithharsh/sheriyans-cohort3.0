5e168a93979277563f2f3d06d041727f8e9fcfe4
Harsh Guleria <guleriah59@gmail.com>
2026-09-22 22:09:13 +0530

    feat: add product validator import to product routes

--- pure_backend/07-snitch/server/src/routes/product.routes.js
+++ pure_backend/07-snitch/server/src/routes/product.routes.js
@@ -1,10 +1,11 @@
 import {Router} from "express"
+import createProductValidator from "../validators/product.validator.js"
 
 const router = Router()
 
 // * @POST /api/products
 router.post("/api/products", async (req, res) => {
-  
+
 })
 
 export default router

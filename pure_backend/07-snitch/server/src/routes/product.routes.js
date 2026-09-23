48cb665511dd0e7c16eb686aeb9dda2be2657206
Harsh Guleria <guleriah59@gmail.com>
2026-09-23 20:55:01 +0530

    feat: implement authentication and authorization for product creation route

--- pure_backend/07-snitch/server/src/routes/product.routes.js
+++ pure_backend/07-snitch/server/src/routes/product.routes.js
@@ -1,11 +1,18 @@
-import {Router} from "express"
-import createProductValidator from "../validators/product.validator.js"
+import { Router } from "express";
+import createProductValidator from "../validators/product.validator.js";
+import { authenticate } from "../middlewares/auth.middleware.js";
 
-const router = Router()
+const router = Router();
 
 // * @POST /api/products
-router.post("/api/products", async (req, res) => {
+router.post("/", authenticate, async (req, res, next) => {
+  if (req.user.role !== "seller") {
+    return res.status(403).json({
+      message: "User is not authorize to create products",
+    });
+  }
 
-})
+  next();
+});
 
-export default router
+export default router;

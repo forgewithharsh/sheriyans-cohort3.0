26ca403551a1c394f67273220d74a2720a3f24be
Harsh Guleria <guleriah59@gmail.com>
2026-09-23 21:14:59 +0530

    feat: add createProduct controller and integrate it into product creation route

---
+++ pure_backend/07-snitch/server/src/controller/product.controller.js
@@ -0,0 +1,9 @@
+import productModel from "../models/product.model.js";
+
+export async function createProduct(req, res) {
+  console.log(req.body);
+
+  res.status(200).json({
+    message: "Dummy reponse",
+  });
+}
--- pure_backend/07-snitch/server/src/routes/product.routes.js
+++ pure_backend/07-snitch/server/src/routes/product.routes.js
@@ -1,18 +1,24 @@
 import { Router } from "express";
 import createProductValidator from "../validators/product.validator.js";
 import { authenticate } from "../middlewares/auth.middleware.js";
+import { createProduct } from "../controller/product.controller.js";
 
 const router = Router();
 
 // * @POST /api/products
-router.post("/", authenticate, async (req, res, next) => {
-  if (req.user.role !== "seller") {
-    return res.status(403).json({
-      message: "User is not authorize to create products",
-    });
-  }
+router.post(
+  "/",
+  authenticate,
+  async (req, res, next) => {
+    if (req.user.role !== "seller") {
+      return res.status(403).json({
+        message: "User is not authorize to create products",
+      });
+    }
 
-  next();
-});
+    next();
+  },
+  createProduct,
+);
 
 export default router;

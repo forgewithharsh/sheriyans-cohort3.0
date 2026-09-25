eb2aba45ff68b40f5a69ee0569c2c1bf1353602b
Harsh Guleria <guleriah59@gmail.com>
2026-09-25 16:11:04 +0530

    feat: enhance createProduct route to handle file uploads and parse price/sizes

--- pure_backend/07-snitch/server/src/routes/product.routes.js
+++ pure_backend/07-snitch/server/src/routes/product.routes.js
@@ -21,7 +21,13 @@ router.post(
 
     next();
   },
-  upload,
+  upload.array("images"),
+  (req, res, next) => {
+    req.body.price = JSON.parse(req.body.price);
+    req.body.sizes = JSON.parse(req.body.sizes);
+
+    next();
+  },
   createProduct,
 );
 

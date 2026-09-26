6e5be5b5cd27b53d1a3ac09f35e3f2f127215f76
Harsh Guleria <guleriah59@gmail.com>
2026-09-26 09:23:14 +0530

    feat: set up user model, authentication routes, and validation structure

---
+++ assignments/assignment11/server/src/models/user.model.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    passwordHash: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["user", "seller"],
+      default: "user",
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  { timestamps: true },
+);
+
+const userModel = mongoose.model("users", userSchema);
+
+export default userModel;

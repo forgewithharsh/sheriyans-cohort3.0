bb125769c35307b19584c91a7e0f054435078723
Harsh Guleria <guleriah59@gmail.com>
2026-09-26 09:04:33 +0530

    feat: add ImageKit integration for file uploads in storage service

--- pure_backend/07-snitch/server/src/config/config.js
+++ pure_backend/07-snitch/server/src/config/config.js
@@ -5,6 +5,10 @@ const config = {
 
   ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET,
   REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET,
+
+  IMAGE_KIT_PUBLIC_KEY: process.env.IMAGE_KIT_PUBLIC_KEY,
+  IMAGE_KIT_PRIVATE_KEY: process.env.IMAGE_KIT_PRIVATE_KEY,
+  IMAGE_KIT_URL_ENDPOINT: process.env.IMAGE_KIT_URL_ENDPOINT,
 };
 
 export default config;
---
+++ pure_backend/07-snitch/server/src/services/storage.service.js
@@ -0,0 +1,17 @@
+import ImageKit, { toFile } from "@imagekit/nodejs";
+import config from "../config/config";
+
+const client = new ImageKit({
+  publicKey: config.IMAGE_KIT_PUBLIC_KEY,
+  privateKey: config.IMAGE_KIT_PRIVATE_KEY,
+  urlEndpoint: config.IMAGE_KIT_URL_ENDPOINT,
+});
+
+export async function uploadFiles({ buffer, fileName }) {
+  const response = await client.files.upload({
+    file: await toFile(buffer),
+    fileName: fileName,
+  });
+
+  return response;
+}
